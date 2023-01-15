84e88e34d650438bb20ef8c3ca1cfa85cb3d0efa
zeri1254 <72800353+zeri1254@users.noreply.github.com>
2023-01-15 15:17:33 +0500

    ToDo nuxt

---
+++ components/Task.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="`task ${task.done ? 'is-complete' : ''}`">
+        <div class="content">{{ task.content }}</div>
+        <div class="buttons">
+            <button @click="ToggleDone">{{ task.done ? 'Undo' : 'Done' }}</button>
+            <button @click="removeTask" class="delete">Delete</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['task'],
+    methods: {
+        ToggleDone(task) {
+            this.$store.commit('TOGGLE_TASK', this.task);
+        },
+        removeTask(task) {
+            this.$store.commit('REMOVE_TASK', this.task);
+        }
+    }
+}
+</script>
+
+<style></style>
